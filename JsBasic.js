0c1f6a6678a567736d4395752220dd7bc6e7bc93
charlmaths <charlzen.martinez@gmail.com>
2024-08-15 11:12:05 +0100

    Add basic console.log examples in JsBasic.js for understanding output in JavaScript

---
+++ JsBasic.js
@@ -0,0 +1,8 @@
+// Printing out in JS, use console.log
+console.log("This is a JS Printout");
+
+// Basic arithmetic in JS, we can also use console.log
+console.log(2 + 2); // This will print out 4
+console.log(20 / 4); // This will print out 5
+console.log(20 / 3); // This will print out 6.666667
+console.log(5 * 4); // This will print out 20
