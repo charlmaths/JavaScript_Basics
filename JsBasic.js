af004fd6ea2bef31174946d7784cc36cc30292eb
charlmaths <charlzen.martinez@gmail.com>
2024-08-15 11:34:56 +0100

    Add basic variable decleration and stored data in JsBasic.js to understand how variables in Js works

--- JsBasic.js
+++ JsBasic.js
@@ -6,3 +6,8 @@ console.log(2 + 2); // This will print out 4
 console.log(20 / 4); // This will print out 5
 console.log(20 / 3); // This will print out 6.666667
 console.log(5 * 4); // This will print out 20
+
+// Basic variable decleration and storing data in them
+let message; // Declare a variable (storage) and named it 'message'
+message = "Data to be stored"; // Now the variable is created, we put data in its memory area/space, we are placing a string "Data to be stored"
+console.log(message); // To print out the data stored
