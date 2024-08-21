dcb8f872092d227e835e0f06e788c04446be4d21
charlmaths <charlzen.martinez@gmail.com>
2024-08-21 12:47:14 +0100

    Update JsBasic.js file,Add JS tasks level-1 from TOP

--- JsBasic.js
+++ JsBasic.js
@@ -11,3 +11,14 @@ console.log(5 * 4); // This will print out 20
 let message; // Declare a variable (storage) and named it 'message'
 message = "Data to be stored"; // Now the variable is created, we put data in its memory area/space, we are placing a string "Data to be stored"
 console.log(message); // To print out the data stored
+
+// JavaScript.info Tasks:
+// Level 1:
+// Declare two variables: admin and name.
+// Assign the value "John" to name.
+// Copy the value from name to admin.
+// Show the value of admin using alert (must output “John”).
+
+let name = "John";
+let admin = name; // Ignore the strikethrough for now - this is for TypeScript, which we will cover later!
+console.log(admin);
